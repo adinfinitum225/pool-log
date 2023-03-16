1f5f48ab97fa7f3e09cc31e72b6cbd0a30f07fa9
Zach Wells <zach.13.wells@gmail.com>
2023-03-16 06:57:13 -0500

    Got basic view working

--- pool_log/__init__.py
+++ pool_log/__init__.py
@@ -28,4 +28,8 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
+    from . import log
+    app.register_blueprint(log.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
---
+++ pool_log/log.py
@@ -0,0 +1,46 @@
+import functools
+
+from flask import(
+        Blueprint, flash, g, redirect, render_template, request, session, url_for
+        )
+
+from pool_log.db import get_db
+
+
+bp = Blueprint('log', __name__)
+
+
+@bp.route('/', methods=('GET', 'POST'))
+def index():
+    db = get_db()
+    logs = db.execute(
+            'SELECT l.id, created, temperature, ph, chlorine, cya, pressure, clarity'
+            ' FROM log l'
+            ' ORDER BY created DESC'
+            ).fetchall()
+    return render_template('log/index.html', logs=logs)
+
+@bp.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        # Get info from the form
+        db = get_db()
+        error = None
+        # Add in errors
+
+        if error is None:
+            try:
+                db.execute(""
+                        # Insert log values into db
+                        )
+                db.commit()
+            except db.IntegrityError:
+                # Add in the error
+                pass
+            else:
+                return redirect(url_for('index'))
+
+            flash(error)
+
+            return render_template('log/create.html')
+
